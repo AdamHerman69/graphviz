194a6222c19e0a2df27fbc3153a093efc9d23394
Adam Herman <adm.herman@gmail.com>
2023-07-09 02:22:31 +0800

    basic node-link working, with no interaction

--- src/components/NodeLink.svelte
+++ src/components/NodeLink.svelte
@@ -1,10 +1,67 @@
-<script>
+<script lang="ts">
 	import Graph from 'graphology';
-	import { VizParams } from '../modules/VizParams';
+	import type { VizParams } from '../modules/VizParams';
+	import { forceSimulation, forceLink, forceManyBody, forceCenter } from 'd3-force';
+	import { onMount } from 'svelte';
 
 	export let graph: Graph;
-	export let vizParams: VizParams;
+	export let vizParams: VizParams = undefined;
+	export let width = 500;
+	export let height = 500;
+
+	console.log(graph);
+	let d3nodes: { name: string }[] = graph.mapNodes((node: string) => ({ name: node }));
+	let d3links: { source: string; target: string }[] = graph.mapEdges(
+		(edgeKey: string, edgeAttributes: object, source: string, target: string) => ({
+			source: source,
+			target: target
+		})
+	);
+
+	let simulation;
+
+	simulation = forceSimulation(d3nodes)
+		.force(
+			'link',
+			forceLink(d3links).id((d) => d.name)
+		)
+		.force('charge', forceManyBody())
+		.force('center', forceCenter(width / 2, height / 2))
+		.on('tick', updateSvg);
+
+	function updateSvg() {
+		d3nodes = d3nodes;
+		d3links = d3links;
+	}
+
+	// onMount(() => {
+	// 	simulation = forceSimulation(d3nodes)
+	// 		.force(
+	// 			'link',
+	// 			forceLink(d3links).id((d) => d.name)
+	// 		)
+	// 		.force('charge', forceManyBody())
+	// 		.force('center', (width / 2, height / 2));
+	// 	simulation.stop();
+	// 	simulation.tick();
+	// });
 </script>
+
+<svg {width} {height}>
+	{#each d3nodes as node}
+		<circle class="node" r="5" fill="pink" cx={node.x} cy={node.y}>
+			<title>{node.name}</title>
+		</circle>
+	{/each}
+
+	{#each d3links as link}
+		<g stroke="#999" stroke-opacity="0.6">
+			<line x1={link.source.x} y1={link.source.y} x2={link.target.x} y2={link.target.y}>
+				<title>{link.source.id}</title>
+			</line>
+		</g>
+	{/each}
+</svg>
 
 <style>
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,36 @@
+<script>
+	import NodeLink from '../components/NodeLink.svelte';
+	import Graph from 'graphology';
+
+	// example graph init
+	const graph = new Graph();
+	graph.addNode('a');
+	graph.addNode('b');
+	graph.addNode('c');
+	graph.addNode('d');
+	graph.addNode('e');
+	graph.addNode('f');
+	graph.addNode('g');
+	graph.addNode('h');
+	graph.addNode('i');
+	graph.addNode('j');
+	graph.addEdge('a', 'b');
+	graph.addEdge('a', 'c');
+	graph.addEdge('a', 'd');
+	graph.addEdge('a', 'e');
+	graph.addEdge('b', 'c');
+	graph.addEdge('g', 'c');
+	graph.addEdge('f', 'd');
+	graph.addEdge('d', 'c');
+	graph.addEdge('e', 'f');
+	graph.addEdge('e', 'g');
+	graph.addEdge('h', 'c');
+	graph.addEdge('i', 'd');
+	graph.addEdge('j', 'c');
+	graph.addEdge('h', 'i');
+	graph.addEdge('j', 'i');
+</script>
+
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto flex justify-center items-center">
@@ -5,9 +38,18 @@
 		<h1 class="h1">Let's get cracking bones!</h1>
 		<p>Start by exploring:</p>
 		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-			<li><code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-			<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
+			<li>
+				<code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order
+				is critical!
+			</li>
+			<li>
+				<code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not
+				be relevant for your project
+			</li>
+			<li>
+				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
+			</li>
 		</ul>
+		<NodeLink {graph} />
 	</div>
 </div>
